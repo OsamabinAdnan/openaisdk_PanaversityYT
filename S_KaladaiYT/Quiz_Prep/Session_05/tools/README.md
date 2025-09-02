15391cddda867caf435ae54c2c7300613e926485
OsamabinAdnan <binadnanosama@gmail.com>
2025-09-02 22:21:54 +0500

    first commit

---
+++ S_KaladaiYT/Quiz_Prep/Session_05/tools/README.md
@@ -0,0 +1,55 @@
+## **Tools**
+
+- **`@function_tool` decorator**
+    - `strick_mode:` By default, it is True
+    - `name_override:`
+    - `description_override:`
+    - `docstring_style:` 3 types of docstrings we can use, see below with example
+        * google
+            ```python
+            def add_numbers(a: int, b: int) -> int:
+                """Add two numbers.
+
+                Args:
+                    a (int): First number.
+                    b (int): Second number.
+
+                Returns:
+                    int: The sum of the two numbers.
+                """
+                return a + b
+            ```
+        * numpy
+            ```python
+            def add_numbers(a: int, b: int) -> int:
+                """Add two numbers.
+
+                Parameters
+                ----------
+                a : int
+                    First number.
+                b : int
+                    Second number.
+
+                Returns
+                -------
+                int
+                    The sum of the two numbers.
+                """
+                return a + b
+            ```
+        * sphinx
+            ```python
+            def add_numbers(a: int, b: int) -> int:
+                """Add two numbers.
+
+                :param a: First number.
+                :type a: int
+                :param b: Second number.
+                :type b: int
+                :return: The sum of the two numbers.
+                :rtype: int
+                """
+                return a + b
+            ```
+    
---
+++ S_KaladaiYT/Quiz_Prep/Session_05/tools/main.py
@@ -0,0 +1,94 @@
+from agents import Agent, Runner, function_tool, OpenAIChatCompletionsModel, RunConfig, enable_verbose_stdout_logging, Tool
+from openai import AsyncOpenAI
+from openai.types.shared import Reasoning
+from dotenv import load_dotenv
+import os
+from rich import print
+from dataclasses import dataclass
+from pydantic import BaseModel
+import requests
+
+load_dotenv()
+
+# set_tracing_disabled(True)
+enable_verbose_stdout_logging()
+
+gemini_api_key=os.getenv("GEMINI_API_KEY")
+if not gemini_api_key:
+    raise Exception("GEMINI_API_KEY is not set. Please set it in the .env file.")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client,
+)
+
+run_config = RunConfig(
+    model=model,
+)
+
+# @function_tool(
+#         strict_mode=False
+# )
+# def get_weather(city: str) -> str:
+#     """
+#     Get the weather for a given city
+
+#     Args:
+#         city (str): The city to get the weather for
+
+#     Returns:
+#         str: The weather for the given city
+#     """
+#     try:
+#         result = requests.get(
+#             f"http://api.weatherapi.com/v1/current.json?key=8e3aca2b91dc4342a1162608252604&q={city}"
+#         )
+
+#         data = result.json()
+
+#         return f"The current weather in {city} is {data["current"]["temp_c"]} C with {data["current"]["condition"]["text"]}."
+    
+#     except Exception as e :
+#         return f"Could not fetch weather data due to {e}"
+
+
+@function_tool(
+        strict_mode=True,
+        name_override="Addition_function",
+        description_override="This function will calculate the sum of two numbers.",
+        docstring_style="google",
+        use_docstring_info=False,
+)
+def add_two_numbers(a: int, b: int) -> str:
+    """Add two numbers.
+    
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+        
+    Returns:
+        str: The sum of the two numbers.
+    """
+
+    result = a + b
+    return f"The sum of {a} and {b} is {result}"
+
+agent = Agent(
+    name="Assitant",
+    instructions="You are a helpful assistant. Give complete answer of user query.",
+    model=model,
+    tools=[add_two_numbers],
+)
+
+result = Runner.run_sync(
+    agent,
+    "What is the sum of 25 and 45?",
+    run_config=run_config,
+)
+
+print(f"\n{result.final_output}\n")
