a216e0029509798690b4a06f10ee9017a028fd4d
OsamabinAdnan <binadnanosama@gmail.com>
2025-10-10 00:47:49 +0500

    first commit

---
+++ CrashCourse_OpenAISDK_Panaversity/Class03_OpenAISDK_CrashCourse/handoffs/main.py
@@ -0,0 +1,66 @@
+import asyncio
+import os
+from typing import Any
+from dotenv import load_dotenv
+from agents import Agent, RunContextWrapper, Runner, OpenAIChatCompletionsModel, enable_verbose_stdout_logging, handoff
+from openai import AsyncOpenAI
+from rich import print
+
+
+load_dotenv()
+
+enable_verbose_stdout_logging()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY environment variable not set")
+
+BASE_URL = os.getenv("BASE_URL")
+if not BASE_URL:
+    raise ValueError("BASE_URL environment variable not set")
+
+# 1. Which LLM Service?
+external_client: AsyncOpenAI = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url=BASE_URL,
+)
+
+# 2. Which LLM Model?
+llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client
+)
+
+refund_agent = Agent(
+    name="Refund agent",
+    instructions="You are a refund agent. Answer related to refund questions.",
+    model=llm_model,
+    handoff_description="You are a refund agent. You work on user refund queries related to order.",
+)
+
+handoff_object = handoff(
+    agent=refund_agent,
+    tool_name_override="order_refund_tool",
+    tool_description_override="You are order refund tool, you work on refund queries related to order.",
+    is_enabled=False,
+)
+
+triage_agent = Agent(
+    name="Triage agent", 
+    handoffs=[handoff_object],
+    instructions="You are a triage agent. You work is to delegate querys to the appropriate agent. Appropriate agents show exist otherwise you will answer him",
+    model=llm_model
+)
+
+async def main():
+    result = await Runner.run(
+        triage_agent, 
+        "I want to refund my order, order details are following: Order ID: 12345, Order amount: $100.00, Order date: 2022-01-01, reason: I don't like the product, and I have product in my hands",
+        # "hi, how are you?"
+    )
+
+    print(f"\nResponse:{result.final_output}")
+    print(f"\nLast agent: {result.last_agent.name}\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
